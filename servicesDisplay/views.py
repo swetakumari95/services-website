e3f822655ef3adb8fdac4bd7e88e9e000693428f
swetakumari95 <sweta191995@gmail.com>
2017-08-13 10:19:26 +0530

    Services list displayed on screen

--- servicesDisplay/views.py
+++ servicesDisplay/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import ServicesList, Services
 
 # Create your views here.
 def service_list(request):
-    return render(request, 'servicesDisplay/service_list.html', {})
+    servicesList = ServicesList.objects.all()
+    return render(request, 'servicesDisplay/service_list.html', {'servicesList': servicesList})
