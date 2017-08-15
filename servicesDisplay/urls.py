0a01dae762d0551a62cb2a34be7836cd893e2d3b
swetakumari95 <sweta191995@gmail.com>
2017-08-15 06:24:57 +0530

    Added details page

--- servicesDisplay/urls.py
+++ servicesDisplay/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'service'
+
 urlpatterns = [
     url(r'^$', views.service_list, name='service_list'),
+    url(r'^(?P<service_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- servicesDisplay/views.py
+++ servicesDisplay/views.py
@@ -5,3 +5,7 @@ from .models import ServicesList, Services
 def service_list(request):
     servicesList = ServicesList.objects.all()
     return render(request, 'servicesDisplay/service_list.html', {'servicesList': servicesList})
+
+def detail(request, service_id):
+     service = Services.objects.get(pk=service_id)
+     return render(request, 'servicesDisplay/detail.html', {'service' : service})
